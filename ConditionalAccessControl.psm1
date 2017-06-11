65133be3c0e6b941e516ef0af761973b18438ad2
Rohn Edwards <rohn.edwards@gmail.com>
2017-06-10 22:48:56 -0500

    Add first conditional ACE commands
    This commit adds the following functions:
      * New-PacConditionalAceCondition
    
        This is a helper function designed to be called when using
        New-PacAccessControlEntry2. It is very bare-bones, and only supports
        creating the MemberOf and MemberOfAny conditions (even though adding the
        DeviceMemberOf and DeviceMemberOfAny should be trivial).
    
      * New-PacAccessControlEntry2
    
        This is a proxy function that wraps New-PacAccessControlEntry and adds a
        -Condition parameter. If that param is used, the function returns a
        CommonAce object instead of the ACE format the PAC module uses. This is
        not a good design, but this is all proof of concept right now anyway.
        Eventually, the New-PacAccessControlEntry (and Add/Remove cmdlets) will
        know how to deal with a -Condition, and this proxy won't be needed.
    
      * Get-PacRawSecurityDescriptor
    
        The PacSecurityDescriptor can't handle conditions right now, and the only
        class that I'm aware of that can is the RawSecurityDescriptor. This thing
        is really raw (you're responsible for the order of the ACEs), so it's
        probably not going to be exported out of the module in a future commit.
    
    I'm leaning towards just creating an Add-PacAccessControlEntry2 function that
    either proxies the original command (like New-PacAccessControlEntry2 is doing),
    or just making a really simple command that takes the ACE objects (principal,
    access mask, flags, etc, including a condition) and the -InputObject, and
    internally it will get the RawSD, insert the ACE where canonical ordering is
    preserved, then make the change to the securable object (it'll act like
    Add-PacAccessControlEntry with the -Apply switch used). This would hide the
    RawSD from the user, even though it would get rid of the ability to do multiple
    SD changes before saving them all out at the end.

---
+++ ConditionalAccessControl.psm1
@@ -0,0 +1,233 @@
+#requires -Version 3
+Import-Module $PSScriptRoot\PowerShellAccessControl
+Add-Type -Path $PSScriptRoot\ConditionalAceTest.cs
+
+function Get-PacRawSecurityDescriptor {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory, ValueFromPipeline)]
+        $InputObject,
+        [switch] $Audit,
+        [ROE.PowerShellAccessControl.PacSdOption] $PacSDOption
+    )
+
+    process {
+        $PacSD = Get-PacSecurityDescriptor @PSBoundParameters
+        New-Object System.Security.AccessControl.RawSecurityDescriptor $PacSD.Sddl
+    }
+}
+
+function New-PacConditionalAceCondition {
+<#
+.SYNOPSIS
+(Incomplete) Function to create conditional ACE condtions.
+
+.DESCRIPTION
+New-PacConditionalAceCondition is used to create ACE conditions that can be passed to the
+-Condition parameter of New-PacAccessControlEntry2.
+
+For a primer on conditional ACEs, see here: https://rohnspowershellblog.wordpress.com/2015/08/29/reading-and-creating-conditional-aces-with-powershell-kind-of/
+
+.EXAMPLE
+New-PacConditionalAceCondition -MemberOf Group1, Group2
+
+This creates an ACE condition that is satisfied if a user is a member of both Group1 and Group2.
+
+.EXAMPLE
+New-PacConditionalAceCondition -MemberOfAny Group1, Group2
+
+This creates an ACE condition taht is satisifed if a user is a member of either Group1 or Group2
+
+.NOTES
+This function can be extended to support more types of conditions.
+#>
+
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory, ParameterSetName='MemberOf')]
+        [string[]] $MemberOf,
+        [Parameter(Mandatory, ParameterSetName='MemberOfAny')]
+        [string[]] $MemberOfAny
+    )
+
+    process {
+        switch ($PSCmdlet.ParameterSetName) {
+
+            { $_ -like '*MemberOf*' } {
+                $OpByteCode = '{0}Member_of{1}' -f "$(if ($_ -like 'Device*') { 'Device_' })", "$(if ($_ -like '*Any') { '_Any' })"
+                $ConditionType = [Testing.ConditionalAceUnaryCondition] 
+
+                $Operator = New-Object Testing.ConditionalAceOperatorToken $OpByteCode
+                $Condition = New-Object $ConditionType $Operator
+
+                # Then create a composite token, which is going to contain the list of SID tokens:
+                $CompositeToken = New-Object Testing.ConditionalAceCompositeToken
+
+                # Find group SIDs (this assumes ParameterSetName is equal to the parameter)
+                # It also assumes only groups are passed (there's no test for this, so a user could be passed, too)
+                foreach ($Group in $PSBoundParameters[$PSCmdlet.ParameterSetName]) {
+                    try {
+                        $Sid = ([System.Security.Principal.NTAccount] $Group).Translate([System.Security.Principal.SecurityIdentifier])
+
+                        $CompositeToken.Tokens.Add((New-Object Testing.ConditionalAceSecurityIdentifierToken $Sid))
+                    }
+                    catch {
+                        Write-Warning "Unable to convert '${Group}' to security identifier: ${_}"
+                    }
+                }
+
+                $Condition.Operand = New-Object Testing.ConditionalAceConditionalLiteralOperand $CompositeToken
+            }
+
+            default {
+                throw "Unknown parameter set name: ${_}"
+            }
+        }
+
+        return $Condition
+    }
+}
+
+<#
+While this may look scary, it's just a proxy function for the PAC module's New-PacAccessControlEntry. This functionality belongs there, but
+there will need to be significant changes made to that module (beyond just this cmdlet) to get it working. Hopefully we can fake it for now
+with the proxy function.
+
+This (hopefully) lets you call New-PacAcessControlEntry2 with a -Condition parameter, and you'll get a CommonAce
+#>
+function New-PacAccessControlEntry2 {
+    [CmdletBinding()]
+    param(
+        [type]
+        ${OutputType},
+
+        [Parameter(ParameterSetName='RegistryRights', ValueFromPipelineByPropertyName=$true)]
+        [Parameter(ParameterSetName='GenericAccessMask', ValueFromPipelineByPropertyName=$true)]
+        [Parameter(ParameterSetName='ActiveDirectoryRights', ValueFromPipelineByPropertyName=$true)]
+        [Parameter(ParameterSetName='ActiveDirectoryRightsObjectAceType', ValueFromPipelineByPropertyName=$true)]
+        [Parameter(ParameterSetName='FolderRights', ValueFromPipelineByPropertyName=$true)]
+        [ROE.PowerShellAccessControl.Enums.AceType]
+        ${AceType},
+
+        [Parameter(ParameterSetName='RegistryRights', Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [Parameter(ParameterSetName='ActiveDirectoryRightsObjectAceType', Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [Parameter(ParameterSetName='GenericAccessMask', Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [Parameter(ParameterSetName='FolderRights', Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [Parameter(ParameterSetName='ActiveDirectoryRights', Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [ROE.PowerShellAccessControl.PacPrincipal]
+        ${Principal},
+
+        [Parameter(ParameterSetName='GenericAccessMask', Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [System.Object]
+        ${AccessMask},
+
+        [Parameter(ParameterSetName='FolderRights', Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [Alias('FileRights','FileSystemRights')]
+        [System.Security.AccessControl.FileSystemRights]
+        ${FolderRights},
+
+        [Parameter(ParameterSetName='RegistryRights', Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [System.Security.AccessControl.RegistryRights]
+        ${RegistryRights},
+
+        [Parameter(ParameterSetName='ActiveDirectoryRights', Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [ROE.PowerShellAccessControl.Enums.ActiveDirectoryRights]
+        ${ActiveDirectoryRights},
+
+        [Parameter(ParameterSetName='GenericAccessMask', ValueFromPipelineByPropertyName=$true)]
+        [Parameter(ParameterSetName='ActiveDirectoryRightsObjectAceType', Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [Parameter(ParameterSetName='ActiveDirectoryRights', ValueFromPipelineByPropertyName=$true)]
+        [ROE.PowerShellAccessControl.ActiveDirectoryAceTypeInstance]
+        ${ObjectAceType},
+
+        [Parameter(ParameterSetName='GenericAccessMask', ValueFromPipelineByPropertyName=$true)]
+        [Parameter(ParameterSetName='ActiveDirectoryRights', ValueFromPipelineByPropertyName=$true)]
+        [Parameter(ParameterSetName='ActiveDirectoryRightsObjectAceType', ValueFromPipelineByPropertyName=$true)]
+        [ROE.PowerShellAccessControl.ActiveDirectoryInheritedAceTypeInstance]
+        ${InheritedObjectAceType},
+
+        [Parameter(ParameterSetName='FolderRights', ValueFromPipelineByPropertyName=$true)]
+        [Parameter(ParameterSetName='ActiveDirectoryRightsObjectAceType', ValueFromPipelineByPropertyName=$true)]
+        [Parameter(ParameterSetName='ActiveDirectoryRights', ValueFromPipelineByPropertyName=$true)]
+        [Parameter(ParameterSetName='GenericAccessMask', ValueFromPipelineByPropertyName=$true)]
+        [Parameter(ParameterSetName='RegistryRights', ValueFromPipelineByPropertyName=$true)]
+        [ROE.PowerShellAccessControl.Enums.AppliesTo]
+        ${AppliesTo},
+
+        [Parameter(ParameterSetName='GenericAccessMask', ValueFromPipelineByPropertyName=$true)]
+        [Parameter(ParameterSetName='RegistryRights', ValueFromPipelineByPropertyName=$true)]
+        [Parameter(ParameterSetName='ActiveDirectoryRights', ValueFromPipelineByPropertyName=$true)]
+        [Parameter(ParameterSetName='FolderRights', ValueFromPipelineByPropertyName=$true)]
+        [Parameter(ParameterSetName='ActiveDirectoryRightsObjectAceType', ValueFromPipelineByPropertyName=$true)]
+        [System.Security.AccessControl.AuditFlags]
+        ${AuditFlags},
+        
+        [Testing.ConditionalAceCondition] $Condition
+    )
+
+    begin
+    {
+        try {
+            $outBuffer = $null
+            if ($PSBoundParameters.TryGetValue('OutBuffer', [ref]$outBuffer))
+            {
+                $PSBoundParameters['OutBuffer'] = 1
+            }
+            $wrappedCmd = $ExecutionContext.InvokeCommand.GetCommand('ConditionalAccessControl\New-PacAccessControlEntry', [System.Management.Automation.CommandTypes]::Cmdlet)
+
+            $scriptCmd = if ($PSBoundParameters.ContainsKey('Condition')) {
+                $null = $PSBoundParameters.Remove('Condition')
+                { 
+                    & $wrappedCmd @PSBoundParameters | ForEach-Object {
+                        $PacAce = $_
+                        $CommonAce = $PacAce -as [System.Security.AccessControl.CommonAce]
+
+                        if ($null -eq $CommonAce) {
+                            throw "Unable to convert PAC ACE to CommonAce!"
+                        }
+
+                        New-Object System.Security.AccessControl.CommonAce (
+                            $CommonAce.AceFlags,
+                            $CommonAce.AceQualifier,
+                            $CommonAce.AccessMask,
+                            $CommonAce.SecurityIdentifier,
+                            $true,
+                            $Condition.GetApplicationData()
+                        )
+                    }
+                }
+            }
+            else {
+                {& $wrappedCmd @PSBoundParameters }
+            }
+            $steppablePipeline = $scriptCmd.GetSteppablePipeline($myInvocation.CommandOrigin)
+            $steppablePipeline.Begin($PSCmdlet)
+        } catch {
+            throw
+        }
+    }
+
+    process
+    {
+        try {
+            $steppablePipeline.Process($_)
+        } catch {
+            throw
+        }
+    }
+
+    end
+    {
+        try {
+            $steppablePipeline.End()
+        } catch {
+            throw
+        }
+    }
+    <#
+
+    .ForwardHelpTargetName ConditionalAccessControl\New-PacAccessControlEntry
+    .ForwardHelpCategory Cmdlet
+
+    #>
+}
